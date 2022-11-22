330ea36da17ca36fe81d7eeb1beac61cc82802ea
CleveHardcastle <treyhardcastle@gmail.com>
2022-11-22 14:31:57 -0800

    youtube api

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,10 @@
+url =
+  "https://youtube.googleapis.com/youtube/v3/search?key=AIzaSyAHEGZXjoIAQqhuRGwRo0koBdvnGGIPul0&q=surfing";
 
+fetch(url)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
