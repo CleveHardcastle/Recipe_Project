00f49224796c8209a4ed94d7636a0acaac292b4e
CleveHardcastle <treyhardcastle@gmail.com>
2022-11-23 11:07:31 -0800

    youtube api

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,20 @@
-url =
-  "https://youtube.googleapis.com/youtube/v3/search?key=AIzaSyAHEGZXjoIAQqhuRGwRo0koBdvnGGIPul0&q=surfing";
+var ytUrl =
+  "https://youtube.googleapis.com/youtube/v3/search?key=AIzaSyAHEGZXjoIAQqhuRGwRo0koBdvnGGIPul0&q=";
 
-fetch(url)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  });
+var btn = document.getElementById("btn");
+var query = document.getElementById("query");
+
+function getInput(event) {
+  event.preventDefault();
+  userInput = query.value;
+  ytSearch = ytUrl + userInput;
+  fetch(ytSearch)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+}
+
+btn.addEventListener("click", getInput);
