5ef3e1bb7f2936b079ab54480445e9934ca32a6b
CleveHardcastle <treyhardcastle@gmail.com>
2022-11-23 13:51:13 -0800

    youtube videos embedded

--- assets/script.js
+++ assets/script.js
@@ -3,14 +3,6 @@ var ytUrl =
 
 var randomUrl = "https://www.themealdb.com/api/json/v1/1/random.php";
 
-fetch(randomUrl)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data.meals[0].strMeal);
-  });
-
 var btn = document.getElementById("btn");
 var query = document.getElementById("query");
 
@@ -18,15 +10,19 @@ function getInput(event) {
   event.preventDefault();
   userInput = query.value.replace(/\s/g, "+");
   ytSearch = ytUrl + userInput + "recipe";
+  setUrl();
+}
+
+function setUrl() {
   fetch(ytSearch)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
       videoId = data.items[0].id.videoId;
-      ytEndUrl = "https://www.youtube.com/watch?v=" + videoId;
+      ytEndUrl = "https://www.youtube.com/embed/" + videoId;
       console.log(ytEndUrl);
+      document.getElementById("player").setAttribute("src", ytEndUrl);
     });
 }
 
