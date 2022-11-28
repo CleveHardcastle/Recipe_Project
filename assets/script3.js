d004b264c1ba08a813ba4e30bc97e50b86e2445f
omousa98 <omarmousa@outlook.com>
2022-11-28 14:11:16 -0800

    DarkMode

--- assets/script3.js
+++ assets/script3.js
@@ -2,12 +2,13 @@ const checkbox = document.querySelector("checkbox");
 const html = document.querySelector("html");
 
 const toggleDarkMode = function () {
-  checkbox.checked
+  checkbox.check
   ? html.classList.add("dark")
   : html.classList.remove("dark");
+  console.log("hello")
 }
 
 //calling the function directly
 
-toggleDarkMode();
-checkbox.addEventListener("click",toggleDarkMode);
+   toggleDarkMode();
+  checkbox.addEventListener("click",toggleDarkMode);
