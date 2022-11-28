e14ed828d49d7ab64642e48af8b1ac2025075149
CleveHardcastle <treyhardcastle@gmail.com>
2022-11-28 11:52:00 -0800

    boo

--- assets/script3.js
+++ assets/script3.js
@@ -1,13 +1,1 @@
-const checkbox = document.querySelector("checkbox");
-const html = document.querySelector("html");
 
-const toggleDarkMode = function () {
-  checkbox.checked
-  ? html.classList.add("dark")
-  : html.classList.remove("dark");
-}
-
-//calling the function directly
-
-toggleDarkMode();
-checkbox.addEventListener("click",toggleDarkMode);
